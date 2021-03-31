10e2e40ccd6e097a1b77cc0ce6c22eb32fcefb6f
Zhang Maiyun <myzhang1029@hotmail.com>
2021-03-31 21:31:01 +0800

    Add new formula

---
+++ Formula/sibsecsh.rb
@@ -0,0 +1,17 @@
+class Sibsecsh < Formula
+  desc "Secure 2FA shell in Rust"
+  homepage "https://github.com/myzhang1029/sibsecsh"
+  url "https://github.com/myzhang1029/sibsecsh/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "2260018751f78ade862b79b605b723abe2d6ece246b2f7dbb43e79b2491a8608"
+  license "AGPL-3.0-or-later"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "false"
+  end
+end
