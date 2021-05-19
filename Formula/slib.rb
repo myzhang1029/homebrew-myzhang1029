4f02809a5da1dd5fef2ef2c54c21c6d7620e3ce8
Zhang Maiyun <myzhang1029@hotmail.com>
2021-05-19 17:06:05 +0800

    Update slib to v4.5.2

--- Formula/slib.rb
+++ Formula/slib.rb
@@ -1,8 +1,8 @@
 class Slib < Formula
   desc "Portable programming interface library written in C for any usage"
   homepage "https://github.com/myzhang1029/slib"
-  url "https://github.com/myzhang1029/slib/archive/refs/tags/v4.5.1.tar.gz"
-  sha256 "a6502560fc40efa283aa4f9635e069e8a5af9ef4f1721df28c13dac2a86bd354"
+  url "https://github.com/myzhang1029/slib/archive/refs/tags/v4.5.2.tar.gz"
+  sha256 "8fd681cde527805607b7348cce153413f25b920a953601af506e7c2dfe1b7f33"
   license "LGPL-3.0-or-later"
 
   depends_on "cmake" => :build
