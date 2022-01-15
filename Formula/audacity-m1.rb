f33d4b86936935c63330e3005a26b94bf3e00caa
Zhang Maiyun <myzhang1029@hotmail.com>
2022-01-15 08:34:12 -0800

    (audacity-m1) Fix style offenses

--- Formula/audacity-m1.rb
+++ Formula/audacity-m1.rb
@@ -2,12 +2,14 @@ class AudacityM1 < Formula
   desc "Multi-track audio editor and recorder"
   homepage "https://www.audacityteam.org/"
   url "https://github.com/myzhang1029/audacity-applesilicon/archive/refs/heads/arm64-macos.tar.gz"
-  version "3.1.3-m1-alpha"
+  version "3.1.3-alpha"
+  sha256 :no_check
   license "GPL-2.0-only"
   head "https://github.com/myzhang1029/audacity-applesilicon.git", branch: "arm64-macos"
 
   depends_on "cmake" => :build
   depends_on "conan" => :build
+  depends_on arch: :arm64
   # Optional FFmpeg support, also for M1
   depends_on "myzhang1029/myzhang1029/ffmpeg-audacity" => :recommended
 
