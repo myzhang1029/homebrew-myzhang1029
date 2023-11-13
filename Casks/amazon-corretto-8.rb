b803bf16087da16c2a05ef63237c22d0c45fd719
Zhang Maiyun <me@maiyun.me>
2023-11-13 09:35:41 -0800

    Add amazon corretto 8

---
+++ Casks/amazon-corretto-8.rb
@@ -0,0 +1,24 @@
+cask "amazon-corretto-8" do
+  version "8.392.08.1"
+  sha256 "c9c9d9004ed11487a37a274cb1e230bc90ff730ea78cefd7fcd75c635e82c650"
+
+  url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-aarch64.pkg"
+  name "amazon-corretto-8"
+  desc "OpenJDK distribution from Amazon, version 8"
+  homepage "https://corretto.aws/"
+
+  livecheck do
+    url "https://github.com/corretto/corretto-8"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
+  end
+
+  pkg "amazon-corretto-#{version}-macosx-aarch64.pkg"
+
+  uninstall pkgutil: "com.amazon.corretto.#{version.major}"
+end
