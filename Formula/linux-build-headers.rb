7b5def319e1ec80c3a96550212547277806ac889
Zhang Maiyun <me@maiyun.me>
2026-01-30 08:15:08 -0500

    linux-build-headers use archives

--- Formula/linux-build-headers.rb
+++ Formula/linux-build-headers.rb
@@ -2,41 +2,38 @@ class LinuxBuildHeaders < Formula
   # To be used with HOST_EXTRACFLAGS=-I/opt/homebrew/opt/linux-build-headers/include make LLVM=1
   desc "Header files for building the Linux kernel"
   homepage "https://sourceware.org/glibc"
-  keg_only "Avoid conflict with macOS headers"
-  url "https://example.com/index.html"
-  version "2.42"
+  url "https://ftp.gnu.org/gnu/glibc/glibc-2.43.tar.xz"
+  sha256 "d9c86c6b5dbddb43a3e08270c5844fc5177d19442cf5b8df4be7c07cd5fa3831"
   license "GPL-2.1-only"
+  head "https://sourceware.org/git/glibc.git"
 
-  uses_from_macos "curl"
-
-  def get_file(glibc_path, our_path)
-    system "curl", "-fsSLo", "#{include}/#{our_path}", "https://sourceware.org/git?p=glibc.git;a=blob_plain;f=#{glibc_path};hb=glibc-#{version}"
-  end
+  keg_only "it will conflict with macOS headers"
 
   def install
     arch = Hardware::CPU.arm? ? "aarch64" : "x86_64"
-    mkdir_p "#{include}/bits"
-    mkdir_p "#{include}/gnu"
-    mkdir_p "#{include}/posix/bits"
-    get_file "elf/elf.h", "elf.h"
-    get_file "bits/byteswap.h", "bits/byteswap.h"
-    get_file "string/bits/endian.h", "bits/endian.h"
-    get_file "sysdeps/#{arch}/bits/endianness.h", "bits/endianness.h"
-    get_file "bits/time64.h", "bits/time64.h"
-    get_file "bits/timesize.h", "bits/timesize.h"
-    get_file "include/bits/types.h", "bits/types.h"
-    get_file "bits/typesizes.h", "bits/typesizes.h"
-    get_file "bits/uintn-identity.h", "bits/uintn-identity.h"
-    get_file "sysdeps/#{arch}/bits/wordsize.h", "bits/wordsize.h"
-    get_file "string/byteswap.h", "byteswap.h"
-    get_file "string/endian.h", "endian.h"
-    get_file "sysdeps/generic/features-time64.h", "features-time64.h"
-    get_file "include/features.h", "features.h"
-    get_file "include/gnu/stubs.h", "gnu/stubs.h"
-    get_file "posix/bits/types.h", "posix/bits/types.h"
-    get_file "include/stdc-predef.h", "stdc-predef.h"
+    mkdir_p include/"bits"
+    mkdir_p include/"gnu"
+    mkdir_p include/"posix/bits"
+    cp "elf/elf.h", include/"elf.h"
+    cp "bits/byteswap.h", include/"bits/byteswap.h"
+    cp "string/bits/endian.h", include/"bits/endian.h"
+    cp "sysdeps/#{arch}/bits/endianness.h", include/"bits/endianness.h"
+    cp "bits/time64.h", include/"bits/time64.h"
+    cp "bits/timesize.h", include/"bits/timesize.h"
+    cp "include/bits/types.h", include/"bits/types.h"
+    cp "bits/typesizes.h", include/"bits/typesizes.h"
+    cp "bits/uintn-identity.h", include/"bits/uintn-identity.h"
+    cp "sysdeps/#{arch}/bits/wordsize.h", include/"bits/wordsize.h"
+    cp "string/byteswap.h", include/"byteswap.h"
+    cp "string/endian.h", include/"endian.h"
+    cp "sysdeps/generic/features-time64.h", include/"features-time64.h"
+    cp "include/features.h", include/"features.h"
+    cp "include/gnu/stubs.h", include/"gnu/stubs.h"
+    cp "posix/bits/types.h", include/"posix/bits/types.h"
+    cp "include/stdc-predef.h", include/"stdc-predef.h"
     # Resolve duplicate __int64_t definitions
-    system "sed", "-i.bak", "s/typedef signed long int __int64_t//;s/typedef unsigned long int __uint64_t//", "#{include}/posix/bits/types.h"
+    system "sed", "-i.bak", "s/typedef signed long int __int64_t//;s/typedef unsigned long int __uint64_t//",
+"#{include}/posix/bits/types.h"
   end
 
   test do
